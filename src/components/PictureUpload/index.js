1cf8d383b1fe07bfe3b92c1d0c54e1f84789468a
ayushchou8876 <choudhuryaayush@gmail.com>
2021-01-29 21:46:28 +0530

    Added Upload backend

--- src/components/PictureUpload/index.js
+++ src/components/PictureUpload/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Button } from '@material-ui/core';
 import { DropzoneAreaBase } from 'material-ui-dropzone';
 import Styles from './styles.module.css';
+import firebase from '../../Home/firebase';
 
 class PictureUpload extends React.Component {
     constructor(props) {
@@ -11,6 +12,7 @@ class PictureUpload extends React.Component {
         };
         this.handleChange = this.handleChange.bind(this);
         this.deleteAll = this.deleteAll.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
     }
 
     handleChange(newItems) {
@@ -23,6 +25,7 @@ class PictureUpload extends React.Component {
             });
             //If there are no duplicates then insert the element into the array
             if (!duplicates) {
+                this.uploadFile(newItem.data);
                 items.push(newItem);
             }
         });
@@ -32,6 +35,30 @@ class PictureUpload extends React.Component {
         });
 
         console.log(this.state.items);
+    }
+
+    uploadFile(item){
+        //Get a reference to the storage service, which is used to create references in your storage bucket
+        //Create a storage reference from storage service
+
+        const storageRef = firebase.storage().ref('/gallery/');
+        const imagesRef = storageRef.child(item.file.name);
+
+        // imagesRef.putString("", 'base64', {contentType: 'image/jpg'}).then((snapshot) => {
+        //     console.log('Uploaded a blob or file!');
+        // });
+
+
+    }
+
+    async addInstaceToFirestore(item){
+        const db = firebase.firestore();
+
+        const ref = db.collection('gallery');
+
+        const res = await ref.add({
+            ref: item.ref //Get location of uploaded file
+        })
     }
 
     deleteAll() {
